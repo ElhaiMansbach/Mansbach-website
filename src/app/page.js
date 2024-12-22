77d86897f27ffbca723d12c2c9dd7e00dff7f1a4
Elhai Mansbach <elhaimn@gmail.com>
2024-12-22 14:58:48 +0200

    test

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,5 @@ import Image from "next/image";
 import styles from "./page.module.scss";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-    </div>
-  );
+  return <div className={styles.page}></div>;
 }
